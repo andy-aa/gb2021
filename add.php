966c8f3f4a84a8bd5669281c9f66153c8af73ca6
Tatsiana13 <Bolshakova123456@mail.ru>
2021-02-26 21:06:43 +0300

    Added empty_name_checking

--- add.php
+++ add.php
@@ -1,19 +1,24 @@
 <?php
 session_start();
 include "function.php";
-if (!empty($_POST["msg"])) {
+if (!empty($_POST["name"])) {
+    if (!empty($_POST["msg"])) {
 
-    $badWords = checkCensor($_POST["msg"]);
+        $badWords = checkCensor($_POST["msg"]);
 
-    if (empty($badWords)) {
-        if (!isset($_SESSION["ban_time"])) {
-            saveMessage($_POST["msg"], $_POST["name"]);
+        if (empty($badWords)) {
+            if (!isset($_SESSION["ban_time"])) {
+                saveMessage($_POST["msg"], $_POST["name"]);
+            }
+        } else {
+            $_SESSION["ban_time"] = time();
+            saveBadWordsToLog($badWords);
         }
     } else {
-        $_SESSION["ban_time"] = time();
-        saveBadWordsToLog($badWords);
+        $_SESSION["empty_msg"] = "Сообщение не может быть пустым!";
     }
 } else {
-    $_SESSION["empty_msg"] = "Сообщение не может быть пустым!";
+    $_SESSION["empty_name"] = "Имя не может быть пустым";
 }
+
 header("Location: main.php");
