4a61be8aa642bb9883edd5a1956d69b6d8fc1e36
azranar-66 <74614652+azranar-66@users.noreply.github.com>
2021-02-26 19:12:56 +0300

    added function htmlTable

--- main.php
+++ main.php
@@ -18,23 +18,10 @@
       <div class="col-sm">
       </div>
       <div class="col-6">
-        <table class="table table-bordered border-primary tab text-break">
-          <tr class="rowses">
-            <th class="w-25 p-3">Дата</th>
-            <th class="w-25 p-3">Комментарий</th>
-            <th class="w-25 p-3">Имя</th>
-          </tr>
-          <?php
-
-          include "function.php";
-          foreach (loadMessages() as $row) {
-            echo "<tr><td class='tab-col'>" .  dateFormat($row["date_time"])  . "</td><td>" . smile($row["msg"]) . "</td><td>" . $row["name"] .
-              "</td></tr>";
-          }
-
-          ?>
-        </table>
-        </table>
+        <?php
+        include_once "ux.php";
+        htmlTable();
+        ?>
         <form action="add.php" method="post">
           <div class="form-floating">
             <textarea name="msg" class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
